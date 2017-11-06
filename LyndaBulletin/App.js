7a17c8770c098827da6f31a0bcef545df6c9f21a
Tanzim Mokammel <mtanzim@gmail.com>
2017-11-06 12:08:47 +0800

    adding update and remove capabilities

--- LyndaBulletin/App.js
+++ LyndaBulletin/App.js
@@ -1,18 +1,52 @@
 
+
+/*
+import React, { PropTypes } from 'react';
+
+
+Board.propTypes = {
+	numNotes: PropTypes.number.isRequired
+};
+*/
+
+
+
 class Board extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notes: ['Hello Mom',
-							'Hello Dad'
-						]
+			note: [
+	      {id: 0, note: 'Call Bob'},
+	      {id: 1, note: 'Email Sarah'},
+	      {id: 2, note: 'Eat Lunch'},
+	      {id: 3, note: 'Finish proposal'}
+      ]
 		};
 	}
+	update (newText, id	) {
+		var notesUpdated = this.state.note.map(
+			note => (note.id !== id) ?
+				note :
+				{...note,note:newText} 
+			)
+		this.setState({notesUpdated});
+	}
+	remove (id) {
+		var notesUpdated = this.state.note.filter((note) => (note.id !== id));
+		this.setState({notesUpdated});
+	}
+
   render() {
     // change code below this line
 		return (
 			<div className='board'>
-				<Note/>
+				{this.state.note.map( (notes) => {
+					return <Note key={notes.id}
+											 note={notes.note}
+											 onChange={this.update}
+											 onRemove={this.remove}
+								  ></Note>
+				})}
 			</div>
 		);
   }
@@ -22,8 +56,12 @@ class Board extends React.Component {
 class Note extends React.Component {
 	constructor(props) {
 		super(props);
+
+
+
 		this.state = {
-			editing: false
+			editing: false,
+			textContent:this.props.note
 		};
 		this.edit = this.edit.bind(this);
 		this.remove = this.remove.bind(this);
@@ -41,14 +79,17 @@ class Note extends React.Component {
 	}
 	remove() {
 		alert("Removing Note");
+		this.props.onRemove(this.props.id);
 	}
 	save () {
 		//var val = this.refs.newText.value;
 		//alert(val);
+		this.props.onChange(this.refs.newText.value, this.props.id);
 		this.setState ({
-			editing:false,
-			textContent:this.refs.newText.value
+			editing:false
 		});
+		
+		
 	}
 	renderForm () {
 		return(
@@ -80,4 +121,4 @@ class Note extends React.Component {
 
 
 
-ReactDOM.render(<Board/>, document.getElementById('react-container'));
+ReactDOM.render(<Board numNotes={100}/>, document.getElementById('react-container'));
