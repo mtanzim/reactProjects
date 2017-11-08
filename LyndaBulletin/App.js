0dbf9931b6d0906196807fc560ed63b80e106992
Tanzim Mokammel <mtanzim@gmail.com>
2017-11-08 14:06:02 +0800

    updated board to allow randonly positioned and draggable notes

--- LyndaBulletin/App.js
+++ LyndaBulletin/App.js
@@ -1,15 +1,7 @@
-
-
-/*
-import React, { PropTypes } from 'react';
-
-
-Board.propTypes = {
-	numNotes: PropTypes.number.isRequired
-};
+/*Tanzim Mokammel
+mtanzim@gmail.com
+Nov 2017
 */
-
-
 
 class Board extends React.Component {
 	constructor(props) {
@@ -25,7 +17,14 @@ class Board extends React.Component {
 		this.add = this.add.bind(this);
 		this.clearAll = this.clearAll.bind(this);
 	}
+	componentWillMount () {
+		//alert('Board loading');
+	}
+	componentDidMount () {
+		//alert('Board loaded');
+	}
 
+	
 
 	clearAll() {
 		this.setState({
@@ -35,7 +34,7 @@ class Board extends React.Component {
 	}
 	add () {
 		//use time as the id as a hack for now
-		var notesUpdated = this.state.note.concat([{note:'New Note', id:(new Date).getTime()}]);
+		var notesUpdated = this.state.note.concat([{id:(new Date).getTime()}]);
 		console.log(this.state.note);
 		
 		this.setState({
@@ -74,9 +73,12 @@ class Board extends React.Component {
 						key={note.id}
 						id={note.id}
 						note={note.note}
+						noteStyle={{backgroundColor:'yellow'}}
 						onChange={this.update}
-						onRemove={this.remove}>
-						{note.note}
+						onRemove={this.remove}
+						onClick={this.changeColor}
+						>
+						{/*note.note*/}
 						</Note>);
 	}
 
@@ -84,9 +86,9 @@ class Board extends React.Component {
     // change code below this line
 		return (
 			<div className='board'>
-				<p>{this.state.numNotes}</p>
-				<button onClick={this.add}>Add</button>
-				<button onClick={this.clearAll}>Clear</button>
+				{/*	<p>{this.state.numNotes}</p>*/}
+				<button id='addBtn' onClick={this.add}>+</button>
+				<button id='clearBtn' onClick={this.clearAll}>Clear</button>
 				{this.state.note.map(this.eachNote)}
 			</div>
 		);
@@ -94,20 +96,60 @@ class Board extends React.Component {
 }
 
 
+
+
 class Note extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			editing: false,
-			textContent:this.props.note
+			textContent:this.props.note,
+			styleState: this.props.noteStyle
 		};
 		this.edit = this.edit.bind(this);
 		this.remove = this.remove.bind(this);
 		this.renderDisplay = this.renderDisplay.bind(this);
 		this.renderForm = this.renderForm.bind(this);
 		this.save = this.save.bind(this);
+		this.changeColor = this.changeColor.bind(this);
+		this.randomBetween = this.randomBetween.bind(this);
 
 	}
+	randomBetween() {
+		var bodyWidth = document.body.clientWidth;
+  	var bodyHeight = document.body.clientHeight;
+  	var randPosX = Math.floor((Math.random()*bodyWidth));
+  	var randPosY = Math.floor((Math.random()*bodyHeight));
+  	console.log({'x':randPosX,'y':randPosY});
+  	return {'x':randPosX,'y':randPosY};
+	}
+	
+	componentWillMount () {
+			this.setState({
+				styleState:{
+					backgroundColor:'yellow',
+					left: this.randomBetween().x+'px',
+          bottom: this.randomBetween().y+'px' 
+				}
+			})
+	}
+	
+	componentWillUpdate () {
+		//console.log ('changing colors');
+	}
+	componentDidUpdate() {
+		if (this.state.editing){
+			this.refs.newText.focus();
+			this.refs.newText.select();
+		}
+	}
+	/*
+	shouldComponentUpdate (nextProps, nextState) {
+		if (this.state.editing){
+			return this.props.note !== nextProps.note || this.state.textContent !== nextState.textContent;
+		} else { return true;}
+	}
+	*/
 
 	edit () {
 		this.setState ({
@@ -129,9 +171,19 @@ class Note extends React.Component {
 		
 		
 	}
+	changeColor () {
+
+		this.state.styleState.backgroundColor === 'purple' ? 
+			this.setState({
+				styleState:{backgroundColor:'blue'}
+			}) :
+			this.setState({
+				styleState:{backgroundColor:'purple'}
+			}) 
+	}
 	renderForm () {
 		return(
-			<div className='note'>
+			<div className='note' style={this.state.styleState} >
 				<textarea ref='newText'>{this.props.note}</textarea>
 				<button onClick={this.save}>SAVE</button>
 			</div>
@@ -140,7 +192,7 @@ class Note extends React.Component {
 	}
 	renderDisplay () {
     return (
-	    <div className="note">
+	    <div className='note' style={this.state.styleState}>
 	    	{/*<p>{this.props.id}</p>*/}
 	    	<p>{this.props.note}</p>
 	    	<span>
@@ -151,13 +203,13 @@ class Note extends React.Component {
     );
 	}
   render() {
-    // change code below this line
-	return (this.state.editing) ? this.renderForm() : this.renderDisplay();
-    // change code above this line
+		return (<ReactDraggable>{((this.state.editing) ? this.renderForm() : this.renderDisplay())}</ReactDraggable>);
   }
 };
 
-
+Note.defaultProps = {
+	note: 'New Note'
+}
 
 
 ReactDOM.render(<Board/>, document.getElementById('react-container'));
